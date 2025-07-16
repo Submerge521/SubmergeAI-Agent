bfc4537e332465e97079df7387efa3ecf045d432
Submerge <2821410432@qq.com>
2025-07-16 11:33:51 +0800

    自定义系统提示词模板

---
+++ Sub-ai-agent/src/main/java/org/submerge/subaiagent/demo/templete/PromptTemplate.java
@@ -0,0 +1,58 @@
+package org.submerge.subaiagent.demo.templete;
+
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplateActions;
+import org.springframework.ai.chat.prompt.PromptTemplateMessageActions;
+import org.springframework.ai.model.Media;
+
+import java.util.List;
+import java.util.Map;
+
+public class PromptTemplate implements PromptTemplateActions, PromptTemplateMessageActions {
+    @Override
+    public Prompt create() {
+        return null;
+    }
+
+    @Override
+    public Prompt create(ChatOptions modelOptions) {
+        return null;
+    }
+
+    @Override
+    public Prompt create(Map<String, Object> model) {
+        return null;
+    }
+
+    @Override
+    public Prompt create(Map<String, Object> model, ChatOptions modelOptions) {
+        return null;
+    }
+
+    @Override
+    public String render() {
+        return "";
+    }
+
+    @Override
+    public String render(Map<String, Object> model) {
+        return "";
+    }
+
+    @Override
+    public Message createMessage() {
+        return null;
+    }
+
+    @Override
+    public Message createMessage(List<Media> mediaList) {
+        return null;
+    }
+
+    @Override
+    public Message createMessage(Map<String, Object> model) {
+        return null;
+    }
+}
--- Sub-ai-agent/src/test/java/org/submerge/subaiagent/app/DataETLAPPTest.java
+++ Sub-ai-agent/src/test/java/org/submerge/subaiagent/app/DataETLAPPTest.java
@@ -1,13 +1,21 @@
 package org.submerge.subaiagent.app;
 
+import cn.hutool.extra.template.TemplateEngine;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.client.RestTemplate;
 import org.submerge.subaiagent.entity.ChatMessage;
 
-import java.util.Date;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +34,11 @@ class DataETLAPPTest {
     @Resource
     private DataETLAPP dataETLAPP;
 
+    /**
+     * 模板文件，这里是读取不到的，需要放在target文件目录下
+     */
+//    @Value("classpath:templates/data-modeling-specialized-template.st")
+//    private ClassPathResource templateResource;
     @Test
     void testChat() {
 
@@ -63,5 +76,43 @@ class DataETLAPPTest {
         Assertions.assertNotNull(report);
     }
 
+    /**
+     * 测试提示词模板
+     */
+    @Test
+    void testPromptTemplate() {
+        String chatId = UUID.randomUUID().toString();
+        ClassPathResource templateResource = new ClassPathResource("templates/data-modeling-specialized-template.st");
 
+        Message systemMessage = getMessage(templateResource);
+
+        // 4. 构建用户查询
+        Message userMessage = new UserMessage("如何设计支持订单分析的维度模型？");
+
+        // 5. 组合为完整Prompt
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        String doChat = dataETLAPP.doChat(String.valueOf(userMessage), chatId);
+        Assertions.assertNotNull(doChat);
+    }
+
+    private static Message getMessage(ClassPathResource templateResource) {
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(templateResource);
+        // 2. 准备测试变量
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("who", "资深的数仓工程师");
+        variables.put("what", "各种业务的维度建模，建设出可复用的数据模型，支持后续业务操作");
+        variables.put("context", "用户的业务领域、数据规模、目标受众");
+        variables.put("status", "当前使用的数据源、已有的数据表、存在的业务系统");
+        variables.put("requirements", "期望的结果");
+        // 生成系统消息
+        Message systemMessage = systemPromptTemplate.createMessage(variables);
+        // 构建用户消息
+        Message userMessage = new UserMessage("{user_query}");
+        // 组合为完整Prompt
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        // 3. 创建系统消息
+        return systemPromptTemplate.createMessage(variables);
+    }
 }
+
+
